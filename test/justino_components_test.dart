30efd06d60e8a789e25b4c02a3b8aaaa7e454029
Rafael Justino <rafael01justino@gmail.com>
2021-03-25 13:11:04 -0300

    [example] Updated example test

--- test/justino_components_test.dart
+++ test/justino_components_test.dart
@@ -1,13 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:justino_components/justino_components.dart';
-
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    final num1 = 1;
+    final num2 = 2;
+    final sum = num1 + num2;
+    expect(sum, 3);
   });
 }
