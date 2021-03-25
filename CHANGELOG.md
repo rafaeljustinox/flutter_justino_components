75d8e39e103e8598b2c3fa3cac56e01fa13091be
Rafael Justino <rafael01justino@gmail.com>
2021-03-25 14:50:07 -0300

    [0.0.4] See CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,15 @@
-## [0.0.3] - 03/25/2021.
+## [0.0.4] - 03/25/21
+
+* Fix: Exports HideKeyboardOnTap and StepperCounter
+
+## [0.0.3] - 03/25/21
 
 * Feature: Added HideKeyboardOnTap and StepperCounter
 
-## [0.0.2] - 03/25/2021.
+## [0.0.2] - 03/25/21
 
 * Fixes: Replaced RoundedTextField with ModernTextField
 
-## [0.0.1] - 03/25/2021.
+## [0.0.1] - 03/25/21
 
 * First version
--- lib/justino_components.dart
+++ lib/justino_components.dart
@@ -4,6 +4,7 @@ export './src/buttons/rounded_button.dart';
 
 export './src/buttons/bottom_button_widget.dart';
 export './src/buttons/corner_floating_action_button.dart';
+export './src/buttons/stepper_counter.dart';
 
 export './src/informative/my_snack_bar.dart';
 export './src/informative/offline_widget.dart';
@@ -13,3 +14,4 @@ export './src/informative/section_title.dart';
 export './src/inputs/modern_text_field.dart';
 
 export './src/structural/behavior_without_glow.dart';
+export './src/structural/hide_keyboard_on_tap.dart';
