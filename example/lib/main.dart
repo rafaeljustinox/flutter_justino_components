898c8cb97614b89571dd700b1b2d77aabe007d41
Rafael Justino <rafael01justino@gmail.com>
2021-03-25 13:09:21 -0300

    [refactoring] Removed debug banner

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Justino Components Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
