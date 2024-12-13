426c8f3a693d5a39de82ad59a9c4d99a749a9ff9
Rafael Justino <rafael01justino@gmail.com>
2024-12-13 11:06:25 -0300

    feat: flutter 3.3.0 stabilized

--- lib/src/structural/behavior_without_glow.dart
+++ lib/src/structural/behavior_without_glow.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BehaviorWithoutGlow extends ScrollBehavior {
+  const BehaviorWithoutGlow();
   @override
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
